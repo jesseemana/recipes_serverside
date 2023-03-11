33ab8bba1c87fe95ef4eae4d8030250bd239090d
Jesse Emana <jesseemana@gmail.com>
2023-03-11 23:14:02 +0200

    added db connection and user creation

--- controllers/user.js
+++ controllers/user.js
@@ -1,19 +1,47 @@
 const User = require('../models/users')
+const bcrypt = require('bcrypt');
 
 
-const getUSers = (req, res) => {
-    res.json({message: 'User found'})
+const getUSers = async(req, res) => {
+    const users = await User.find()
+
+    if(!users?.length) return res.status(400).json({message: 'Users not found'})
+
+    res.json(users)
 };
 
-const registerUSer = (req, res) => {
-    res.json({message: 'User created'});
+const registerUSer = async(req, res) => {
+    const {username, email, password} = req.body
+
+    if(!username || !email || !password) return res.status(400).json({message: 'Please fill out all fields'})
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    
+    const duplicate = await User.findOne({email})
+
+    if(duplicate) return res.status(409).json({message: 'Account already exist'}).collation({locale: 'en', strength: 2}).lean().exec();
+
+
+    const newUser = {
+        username: username,
+        email: email,
+        password: hashedPassword
+    }
+
+    const user = await User.create(newUser)
+
+    if(user) {
+        return res.status(200).json({message: `New user ${username} has been created`})
+    } else {
+        res.status(400).json({message: 'Invalid user data received'});
+    }
 };
 
-const updateUser = (req, res) => {
+const updateUser = async(req, res) => {
     res.json({message: 'User updated'});
 };
 
-const deleteUser = (req, res) => {
+const deleteUser = async(req, res) => {
     res.json({message: 'User deleted'});
 };
 
--- server.js
+++ server.js
@@ -1,27 +1,56 @@
 require('colors')
 require('dotenv').config();
 require('express-async-errors');
+const path = require('path');
 const cors = require('cors');
 const helmet = require('helmet');
 const express = require('express');
+const connectDB = require('./config/connectDB')
 const errorHandler = require('./middleware/errorHandler');
 const corsOptions = require('./config/corsOptions');
+const {default: mongoose} = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT;
 
+connectDB()
+
+// MIDDLEWARE 
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('/public'));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
+// ROUTES 
 app.use('/users', require('./routes/users'));
 app.use('/recipes', require('./routes/recipes'));
 
 
+// ERROR 404 PAGE 
+app.use('*', (req, res) => {
+    res.status(404)
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')) { 
+        res.json({message: '404 Page Not Found'})
+    } else {
+        res.type('txt').send('404 Page Not Found')
+    }
+})
+
+
+// ERROR HANDLER MIDDLEWARE 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`.cyan.underline));
+mongoose.connection.once('open', () => {
+    console.log(`Database connected...`.cyan.underline)
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}...`.cyan.underline));
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+    // logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+});
