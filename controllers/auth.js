92bd0cd761401cc921df5eecfd38bb3e704e83d1
Jesse Emana <jesseemana@gmail.com>
2023-03-12 21:39:06 +0200

    created user authentication

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+
+
+const login = (req, res) => {};
+const refresh = (req, res) => {}
+const logout = (req, res) => {};
+
+module.exports = {
+    login,
+    refresh,
+    logout
+};
--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 const getUSers = async(req, res) => {
     const users = await User.find().select('-password').lean()
-    if(!users?.length) return res.status(400).json({message: 'Users not found'})
+    if(!users?.length) return res.status(400).json({message: 'No users found'})
 
     res.json(users)
 };
@@ -15,11 +15,10 @@ const createUSer = async(req, res) => {
     const {username, email, password} = req.body
     if(!username || !email || !password) return res.status(400).json({message: 'Please fill out all fields'})
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-    
     const duplicate = await User.findOne({email}).collation({locale: 'en', strength: 2}).lean().exec();
-    if(duplicate) return res.status(409).json({message: 'Account already exist'})
+    if(duplicate) return res.status(409).json({message: 'email already exist'})
 
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const newUser = {
         username: username,
@@ -39,22 +38,21 @@ const createUSer = async(req, res) => {
 
 
 const updateUser = async (req, res) => {
-    const {id} = req.params
     const {username, password, role} = req.body
+    const {id} = req.params
 
-    if(!username || !role) return res.status(400).json({message: 'All fields except password are required'})
+    if(!username || !role) return res.status(400).json({message: 'Username and role required'})
 
     const user = await User.findById(id).exec()
     if(!user) return res.status(400).json({message: 'User Not Found'})
 
     const duplicate = await User.findById({id}).collation({locale: 'en', strength: 2}).lean().exec();
-    if(duplicate) return res.status(409).json({message: 'Account already exist'})
+    if(duplicate) return res.status(409).json({message: 'User already exist'})
 
     user.username = username
     user.role = role
 
     const updatedUser = await user.save()
-
 
     // PASSWORD UPDATE FROM FRONTEND 
     if(password)
