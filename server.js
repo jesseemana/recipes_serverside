f476cff47a23e2d0b45241fe541a03c974ed76ae
Jesse Emana <jesseemana@gmail.com>
2023-03-30 17:15:16 +0200

    cleaned up server

--- server.js
+++ server.js
@@ -23,11 +23,6 @@ const PORT = process.env.PORT || 8080;
 
 connectDB();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename)
-// >>>>>>>>>>>>>>>>>>>>> SET SECURE TO TRUE IN AUTH CONTROLLER RES.COOKIE() IN DEPLOYMENT
-//>>>>>>>>>>>>>>>>>>>>>> INSTALL MULTER 
-
 
 // MIDDLEWARE 
 app.use(logger);
@@ -36,7 +31,6 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/assets', express.static(path.join(__dirname, '/public/assets')));
@@ -51,10 +45,7 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
-
 const upload = multer({storage});
-
-// ROUTE FOR CREATING RECIPE WITH PICTURE 
 app.post('/recipes', upload.single('picture'), verifyUser, createRecipe);
 
 
@@ -62,19 +53,6 @@ app.post('/recipes', upload.single('picture'), verifyUser, createRecipe);
 app.use('/auth', require('./routes/auth'));
 app.use('/users', require('./routes/users'));
 app.use('/recipes', require('./routes/recipes'));
-
-
-// ERROR 404 PAGE 
-app.use('*', (req, res) => {
-    res.status(404);
-    if(req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if(req.accepts('json')) {
-        res.json({message: '404 Page Not Found'});
-    } else {
-        res.type('txt').send('404 Page Not Found');
-    }
-});
 
 
 // ERROR HANDLING MIDDLEWARE 
