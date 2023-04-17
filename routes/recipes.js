b8fa2de8956808cd039667923ed0d40a5436d7ad
Jesse Emana <jesseemana@gmail.com>
2023-04-17 21:38:06 +0200

    changed auth, even non logged in users can see recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,12 +3,11 @@ const router = express.Router()
 const recipeController = require('../controllers/recipe')
 const verifyJWT = require('../middleware/auth')
 
-router.use(verifyJWT)
 
 router.route('/')
     .get(recipeController.getRecipes)
-    .patch(recipeController.updatedRecipe)
-    .delete(recipeController.deleteRecipe)
+    .patch(verifyJWT, recipeController.updatedRecipe)
+    .delete(verifyJWT, recipeController.deleteRecipe)
 
 router.route('/:id')
     .get(recipeController.getSingleRecipe)
