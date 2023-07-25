91d7724835f077e5f9e12f7647ab301db84a5a98
Jesse Emana <jesseemana@gmail.com>
2023-07-25 17:31:54 +0200

    formatted code and changed casing style

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization
-    if(!authHeader?.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized' })
+  const auth_header = req.headers.authorization || req.headers.Authorization
+  if (!auth_header?.startsWith('Bearer ')) return res.status(401).json({ message: 'Unauthorized' })
 
-    const token = authHeader.split(' ')[1]
+  const token = auth_header.split(' ')[1]
 
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN,
-        (err, decoded) => {
-            if(err) return res.status(403).json({ message: 'Forbidden' })
-
-            req.user = decoded.email
-            next()
-        }
-    )
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN,
+    (err, decoded) => {
+      if (err) return res.status(403).json({message: 'Forbidden'})
+      req.user = decoded.email
+      next()
+    }
+  )
 }
 
-module.exports = verifyJWT
+module.exports = verifyJWT  
