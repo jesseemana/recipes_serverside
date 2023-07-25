eed14b7d152b76746139f1b1271365e342e49613
Jesse Emana <jesseemana@gmail.com>
2023-07-25 17:47:31 +0200

    created reviews controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,33 +1,29 @@
 const Reviews = require('../models/Review')
 
-
 const getReviews = async (req, res) => {
-    const reviews = await Reviews.find()
-
-    res.status(200).json({reviews})
+  const reviews = await Reviews.find()
+  res.status(200).json({reviews})
 }
 
-
 const createReview = async (req, res) => {
-    const { recipe, review } = req.body
+  const { recipe, review } = req.body
+  if (!review || !recipe)
+    return res.satus(400).json({message: 'Review and recipe fields cannot be blank.'})
 
-    if(!review || !recipe) return res.satus(400).json({message: 'Review and recipe fields cannot be blank.'})
-
-    const new_review = new Reviews({
-        recipe,
-        review,
-    })
-
-    await new_review.save()
-
-    if(new_review) {
-        return res.status(201).json({message: 'Review has been posted.'})
-    } else {
-        return res.status(400).json({message: 'Invalid data received.'})
-    }
+  const new_review = new Reviews({
+    recipe,
+    review,
+  })
+  await new_review.save()
+  
+  if (new_review) {
+    return res.status(201).json({message: 'Review has been posted.'})
+  } else {
+    return res.status(400).json({message: 'Invalid data received.'})
+  }
 }
 
 module.exports = {
-    getReviews,
-    createReview
-}   
+  getReviews,
+  createReview
+}    
