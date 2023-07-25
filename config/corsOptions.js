24fcd368b49ddbac1c5d3a8c177ae02c989ef353
Jesse Emana <jesseemana@gmail.com>
2023-07-25 17:41:12 +0200

    formatted code

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,21 +1,20 @@
 const allowedOrigins = [
+    'http://localhost:5173', // FOR REACT USING VITE, CHANGE IT TO WHATEVER PORT YOUR'E ON IN YOUR LOCALHOST
     'http://localhost:8080',
-    'http://localhost:5173', // FOR REACT WHEN USING VITE, CHANGE IT TO WHATEVER PORT YOUR'E ON IN YOUR LOCALHOST
+    'http://localhost:3000',
     'https://www.mydomainname.com'
 ]
 
-
 const corsOptions = {
-    origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
 };
-
 
 module.exports = corsOptions;
