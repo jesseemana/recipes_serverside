ee912c6d1584122a493080b712f81fd0638dbe5c
Jesse Emana <jesseemana@gmail.com>
2023-08-07 16:30:39 +0200

    configured password reset route

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const passwordController = require('../controllers/resetPassword')
 
-router.route('/forgot-password')
+router.route('/')
   .post(passwordController.forgotPassword)
 
-router.route('/reset-password/:id/:token')
+router.route('/:id/:token')
   .patch(passwordController.resetPassword)
 
 module.exports = router 
