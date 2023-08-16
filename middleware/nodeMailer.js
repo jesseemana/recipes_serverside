c79047ed45ddb6159a89245c2c7b48ca33a3af65
Jesse Emana <jesseemana@gmail.com>
2023-08-16 12:23:55 +0200

    setup for nodemailer transporter

--- middleware/nodeMailer.js
+++ middleware/nodeMailer.js
@@ -1,20 +1,20 @@
-// const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp-mail.outlook.com',
-//   auth: {
-//     user: process.env.AUTH_EMAIL,
-//     pass: process.env.AUTH_PASS
-//   }
-// });
+const transporter = nodemailer.createTransport({
+  host: 'gmail',
+  auth: {
+    user: process.env.AUTH_EMAIL,
+    pass: process.env.AUTH_PASS
+  }
+});
 
-// transporter.verify((error, succes) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Ready for message');
-//     console.log(succes);
-//   }
-// });
+transporter.verify((error, succes) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready for message');
+    console.log(succes);
+  }
+});
 
-// module.exports = transporter; 
+module.exports = transporter; 
