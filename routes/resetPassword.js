29bf2c26c62926400279b9e900e2d1a7d318837a
Jesse Emana <jesseemana@gmail.com>
2023-09-14 23:23:39 +0200

    imported controller with new name

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -1,10 +1,8 @@
 const router = require('express').Router()
 const passwordController = require('../controllers/resetPassword')
 
-router.route('/')
-  .post(passwordController.forgotPassword)
+router.route('/').post(passwordController.sendLink)
 
-router.route('/:id/:token')
-  .patch(passwordController.resetPassword)
+router.route('/:id/:token').patch(passwordController.resetPassword)
 
 module.exports = router 
