3de16798f15d54e7a9486fb2d19e24c9c443aa7a
Jesse Emana <jesseemana@gmail.com>
2023-09-14 23:30:51 +0200

    uncomented out the reset route

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./config/connectDB');
 const errorHandler = require('./middleware/errorHandler');
 const corsOptions = require('./config/corsOptions');
-const { logger, logEvents } = require('./middleware/logger'); // Morgan can also be used for logging
+const { logger, logEvents } = require('./middleware/logger'); // CUSTOM LOGGER, MORGAN CAN ALSO BE USED
 const path = require('node:path');
 const cpus = require('node:os').cpus();
 const cluster = require('node:cluster');
 const cloudinary = require('./utils/cloudinary');
 const verifyToken = require('./middleware/auth');
-const upload = require('./middleware/multer')
 require('colors');
 require('dotenv').config();
 require('express-async-errors');
@@ -26,8 +25,8 @@ connectDB();
 // MIDDLEWARE 
 app.use(logger);
 app.use(helmet());
-app.use(cors(corsOptions));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '50MB' }));
 app.use(express.urlencoded({ limit: '50MB', extended: true }));
 app.use('/', express.static(path.join(__dirname, '/public')));
@@ -37,12 +36,11 @@ app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/recipes', require('./routes/recipes'));
 app.use('/api/v1/reviews', require('./routes/reviews'));
 app.use('/api/v1/bookmarks', require('./routes/bookmarks'));
-// app.use('/api/v1/reset', require('./routes/resetPassword'));
+app.use('/api/v1/reset', require('./routes/resetPassword'));
 
 app.post('/api/v1/upload', verifyToken, async(req, res) => {
-  //  CONFIGURE CLOUDINARY UPLOAD PRESET FIRST
   const response = await cloudinary.uploader.upload(req.body.data, {
-    upload_preset: 'recipes'
+    upload_preset: 'recipes' // CONFIGURE UPLOAD_PRESET IN CLOUDINARY
   }); 
   res.status(200).json({
     id: response.public_id,
@@ -63,7 +61,6 @@ if (cluster.isMaster) {
     cluster.fork();
   })
 } else {
-  // console.log(`Worker ${process.pid} started...`);
   mongoose.connection.once('open', () => {
     console.log(`Database connected...`.cyan.underline);
     app.listen(PORT, () => console.log(`Server #${process.pid} running on port: ${PORT}...`.cyan.underline));
