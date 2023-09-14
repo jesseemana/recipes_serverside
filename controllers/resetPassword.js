a727b8f773b0cfe18c832206cf5dcbd39feb8a31
Jesse Emana <jesseemana@gmail.com>
2023-09-14 23:23:11 +0200

    fixed functionality for updating the users' password

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -1,23 +1,24 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const transporter = require('../utils/nodeMailer')
+// const transporter = require('../utils/nodeMailer')
 
 
-const forgotPassword = async (req, res) => {
+const sendLink = async (req, res) => {
   const { email } = req.body
   if (!email) 
-    return res.status(400).json({ 'message': 'please provide an email adress.' })
+    return res.status(400).json({ message: 'Please provide an email adress.' })
 
   const user = await User.findOne({email}).exec()
   if (!user) 
-    return res.status(401).json({ 'message': `User doesn't exist.` })
+    return res.status(401).json({ message: `User doesn't exist.` })
 
-  // create a one time link valid for 5 minutes(30 minutes in production)
-  const secret = process.env.JWT_SECRET + user.password
-  const token = jwt.sign({'email': user.email}, secret, { expiresIn: '5m' })
-  const link = `http://localhost:5173/reset-password/${user._id}/${token}`
-  console.log(link) // send this link users' email
+  // create a one time link valid for 20 minutes in production
+  const SECRET_TOKEN = process.env.JWT_SECRET + user.password
+  const token = jwt.sign({ 'email': user.email }, SECRET_TOKEN, { expiresIn: '20m' })
+  const link = `http://localhost:8080/api/v1/reset/${user._id}/${token}` // FOR TESTING IN BACKEND(POSTMAN)
+  // const link = `http://localhost:5173/reset-password/${user._id}/${token}` // FOR FRONTEND
+  console.log(link) // send this link to users' email
 
   // NODEMAILER SETUP
   // const mailOptions = {
@@ -29,38 +30,33 @@ const forgotPassword = async (req, res) => {
 
   // await transporter.sendMail(mailOptions)
 
-  res.status(200).json({ 'message': `Password reset link sent to users' email.` })
+  res.status(200).json({ message: `Password reset link sent to users' email.` })
 } 
- 
+
 
 const resetPassword = async (req, res) => {
   const { id, token } = req.params
-  const { password } = req.body
-  if (!id || !token || !password) 
-    return res.status(400).json({ 'message': 'Please provide user id, token and password.' })
+  if (!id || !token || !req.body.password) 
+    return res.status(400).json({ message: 'Please provide user id, token and password!' })
 
   const user = await User.findById(id)
-  if (!user) return res.status(401).json({ 'message': `User doesn't exist.` })
-  console.log(user)
+  if (!user) 
+    return res.status(401).json({ message: `User doesn't exist.` })
 
-  const secret = process.env.JWT_SECRET + user.password
-  
-  const hashed_passord = await bcrypt.hash(password, 10)
+  const SECRET_TOKEN = process.env.JWT_SECRET + user.password
+  const new_password = await bcrypt.hash(req.body.password, 10)
 
-  jwt.verify(
-    token, 
-    secret,
-    async function (err) {
-      if (err) return res.status(403).json({ 'message': 'Forbidded.' })
-      user.password = hashed_passord
+  jwt.verify(token, SECRET_TOKEN, async (err, _) => {
+      if (err) return res.status(403).json({ message: 'Forbidden.' })
+      user.password = new_password
       await user.save()
-      res.json(200).json({ 'message': 'User password has been updated.' })
+      res.status(200).json({ message: 'User password has been updated.' })
     }
   )
 }    
 
 
 module.exports = {
-  forgotPassword,
+  sendLink,
   resetPassword,
-} 
+}   
