50c4dfe474aeaf5465a80cfdd5173c5afb0ed507
Jesse Emana <jesseemana@gmail.com>
2023-09-20 20:07:46 +0200

    changed param names

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -2,11 +2,11 @@ const router = require('express').Router()
 const bookmarksController = require('../controllers/bookmarks')
 const verifyJWT = require('../middleware/auth')
 
-router.route('/:userId')
+router.route('/:id')
   .get(verifyJWT, bookmarksController.userBookmarks)
 
-router.route('/:recipeId/:userId')
+router.route('/:user_id/:recipe_id')
   .post(verifyJWT, bookmarksController.addBookmark)
   .delete(verifyJWT, bookmarksController.removeBookmark)
 
-module.exports = router     
+module.exports = router   
