55588a8ad325e2a8e2572424ad76d10a6cd08e0c
Jesse Emana <jesseemana@gmail.com>
2023-10-06 03:30:27 +0200

    imported routes function

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+require('colors');
+require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const connectDB = require('./config/connectDB');
-const errorHandler = require('./middleware/errorHandler');
-const corsOptions = require('./config/corsOptions');
-const { logger, logEvents } = require('./middleware/logger'); // CUSTOM LOGGER, MORGAN CAN ALSO BE USED
-const path = require('node:path');
 const cpus = require('node:os').cpus();
 const cluster = require('node:cluster');
+const routes = require('./routes');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/connectDB');
 const cloudinary = require('./utils/cloudinary');
 const verifyToken = require('./middleware/auth');
-require('colors');
-require('dotenv').config();
-require('express-async-errors');
+const corsOptions = require('./config/corsOptions');
+const errorHandler = require('./middleware/errorHandler');
+const { logger, logEvents } = require('./middleware/logger');
 
 const app = express();
 
@@ -29,24 +29,25 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '50MB' }));
 app.use(express.urlencoded({ limit: '50MB', extended: true }));
-app.use('/', express.static(path.join(__dirname, '/public')));
+
+routes(app);
 
 // ROUTES 
-app.use('/api/v1/auth', require('./routes/auth'));
-app.use('/api/v1/recipes', require('./routes/recipes'));
-app.use('/api/v1/reviews', require('./routes/reviews'));
-app.use('/api/v1/bookmarks', require('./routes/bookmarks'));
-app.use('/api/v1/reset', require('./routes/resetPassword'));
+// app.use('/api/v1/auth', require('./routes/auth'));
+// app.use('/api/v1/recipes', require('./routes/recipes'));
+// app.use('/api/v1/reviews', require('./routes/reviews'));
+// app.use('/api/v1/bookmarks', require('./routes/bookmarks'));
+// app.use('/api/v1/reset', require('./routes/resetPassword'));
 
-app.post('/api/v1/upload', verifyToken, async(req, res) => {
-  const response = await cloudinary.uploader.upload(req.body.data, {
-    upload_preset: 'recipes' // CONFIGURE UPLOAD_PRESET IN CLOUDINARY
-  }); 
-  res.status(200).json({
-    id: response.public_id,
-    url: response.secure_url,
-  });
-});
+// app.post('/api/v1/upload', verifyToken, async(req, res) => {
+//   const response = await cloudinary.uploader.upload(req.body.data, {
+//     upload_preset: 'recipes' // CONFIGURE UPLOAD_PRESET IN CLOUDINARY
+//   }); 
+//   res.status(200).json({
+//     id: response.public_id,
+//     url: response.secure_url,
+//   });
+// });
 
 // ERROR HANDLING MIDDLEWARE 
 app.use(errorHandler);
@@ -59,15 +60,15 @@ if (cluster.isMaster) {
   cluster.on('exit', (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} has died...`);
     cluster.fork();
-  })
+  });
 } else {
   mongoose.connection.once('open', () => {
     console.log(`Database connected...`.cyan.underline);
     app.listen(PORT, () => console.log(`Server #${process.pid} running on port: ${PORT}...`.cyan.underline));
   });
 
-  mongoose.connection.on('error', err => {
+  mongoose.connection.on('error', (err) => {
     console.log(err);
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
   });
-} 
+}   
