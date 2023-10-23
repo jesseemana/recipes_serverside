cdd9b696825efc5b89d4cf7ccc16c289e32ed2e3
Jesse Emana <jesseemana@gmail.com>
2023-10-23 13:20:46 +0200

    imported modules from src folder

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ require('dotenv').config();
 require('express-async-errors');
 const cors = require('cors');
 const helmet = require('helmet');
-const express = require('express');
-const mongoose = require('mongoose');
 const cpus = require('os').cpus();
 const cluster = require('cluster');
-const routes = require('./utils/routes');
+const express = require('express');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const connectDB = require('./config/connectDB');
-const { logger } = require('./middleware/logger');
-const corsOptions = require('./config/corsOptions');
-const errorHandler = require('./middleware/errorHandler');
+
+const routes = require('./src/utils/routes');
+const connectDB = require('./src/config/connectDB');
+const { logger } = require('./src/middleware/logger');
+const corsOptions = require('./src/config/corsOptions');
+const errorHandler = require('./src/middleware/errorHandler');
 
 const PORT = process.env.PORT;
 
@@ -23,7 +24,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '50MB' }));
-app.use(express.urlencoded({ limit: '50MB', extended: true }))
+app.use(express.urlencoded({ limit: '50MB', extended: true }));
 
 connectDB();
 
@@ -45,6 +46,5 @@ if (cluster.isMaster) {
     console.log(`Database connected...`.cyan.underline);
     app.listen(PORT, () => console.log(`Server #${process.pid} running on port: ${PORT}...`.cyan.underline));
   });
-
   mongoose.connection.on('error', (err) => console.log(err));
 }   
