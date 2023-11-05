4c9939fb7a5e00da676e4c9837905d3b572add61
Jesse Emana <jesseemana@gmail.com>
2023-11-05 19:51:58 +0200

    created a logger and zod schema

---
+++ src/schema/schema.ts
@@ -0,0 +1,40 @@
+import { object, string, TypeOf } from 'zod'
+
+export const createUserSchema = object({
+  body: object({
+    first_name: string({
+      required_error: 'First name is required',
+    }),
+    last_name: string({
+      required_error: 'Last name is required',
+    }),
+    password: string({
+      required_error: 'Password is required',
+    }).min(6, 'Password is too short - should be min 6 chars'),
+    confirm_password: string({
+      required_error: 'Password confirmation is required',
+    }),
+    email: string({
+      required_error: 'Email is required',
+    }).email('Not a valid email'),
+  }).refine((data) => data.password === data.confirm_password, {
+    message: 'Passwords do not match',
+    path: ['confirm_password'],
+  }),
+})
+
+
+export const createSessionSchema = object({
+  body: object({
+    email: string({
+      required_error: 'Email is required',
+    }).email('Not a valid email'),
+    password: string({
+      required_error: 'Password is required',
+    }).min(6, 'Password is too short - should be min 6 chars'),
+  })
+})
+
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>['body']
+export type CreateSessionInput = TypeOf<typeof createSessionSchema>['body']
