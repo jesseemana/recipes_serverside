c1d74338136cc57971f8e5ad19ca080f163b5469
Jesse Emana <jesseemana@gmail.com>
2023-11-07 17:22:52 +0200

    changed code

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,8 +1,4 @@
 import { Express, Request, Response } from 'express'
-// @ts-ignore
-import cloudinary from './cloudinary';
-import verifyToken from '../middleware/auth.middleware';
-
 import authRouter from '../routes/auth.route'
 import recipesRoute from '../routes/recipes.route'
 import bookmarksRoute from '../routes/bookmarks.route'
@@ -13,17 +9,6 @@ const routes = (app: Express) => {
   app.use('/api/v1/recipes', recipesRoute);
   app.use('/api/v1/bookmarks', bookmarksRoute);
   app.use('/api/v1/reset', resetRoute);
-
-  // WHEN UPLOADING THE PICTURE SEPARATELY FROM THE FORMDATA 
-  app.post('/api/v1/upload', verifyToken, async (req: Request, res: Response) => {
-    const response = await cloudinary.uploader.upload(req.body.data, {
-      upload_preset: 'recipes' // CONFIGURE UPLOAD_PRESET IN CLOUDINARY
-    });
-    res.status(200).json({
-      id: response.public_id,
-      url: response.secure_url,
-    });
-  });
 };
 
 export default routes
