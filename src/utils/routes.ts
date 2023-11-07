3e7439c1d677c722eba303708814339a01ce8bdb
Jesse Emana <jesseemana@gmail.com>
2023-11-07 22:44:06 +0200

    removed single picture upload route

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,4 +1,4 @@
-import { Express, Request, Response } from 'express'
+import { Express } from 'express'
 import authRouter from '../routes/auth.route'
 import recipesRoute from '../routes/recipes.route'
 import bookmarksRoute from '../routes/bookmarks.route'
