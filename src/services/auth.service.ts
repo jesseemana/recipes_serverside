136518260b5e3f53d95e5721650192b915f7ad25
Jesse Emana <jesseemana@gmail.com>
2023-11-08 20:39:13 +0200

    FEAT: created a service for handling user sessions including tokens

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,8 @@
-import SessionModel from '../models/session.model'
+import SessionModel, { Session } from '../models/session.model'
+import { FilterQuery, UpdateQuery } from 'mongoose'
 import { User } from '../models/user.model'
-import { DocumentType } from '@typegoose/typegoose'
 import { omit } from 'lodash'
+import { DocumentType } from '@typegoose/typegoose'
 import { signJwt } from '../utils/jwt'
 import log from '../utils/logger'
 
@@ -13,7 +14,11 @@ export const findSessionById = async (id: string) => {
   return SessionModel.findById(id)
 }
 
-export const signAccessToken = async (user: DocumentType<User>) => {
+export const updateSession = (query: FilterQuery<Session>, update: UpdateQuery<Session>) => {
+  return SessionModel.findOneAndUpdate(query, update)
+}
+
+export const signAccessToken = (user: DocumentType<User>) => {
   const payload = omit(user.toJSON(), ['password', 'bookmarks'])
   const access_token = signJwt(payload, 'accessTokenPrivateKey', { expiresIn: '1d' })
 
