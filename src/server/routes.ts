22906f9e508795c5a3490ae42f6bf8a7ed5327e3
Jesse Emana <jesseemana@gmail.com>
2023-11-08 14:35:25 +0200

    REFACTOR: created separate folder for configurong routes, server and starting server

---
+++ src/server/routes.ts
@@ -0,0 +1,14 @@
+import { Express } from 'express'
+import authRouter from '../routes/auth.route'
+import recipesRoute from '../routes/recipes.route'
+import bookmarksRoute from '../routes/bookmarks.route'
+import resetRoute from '../routes/reset.route'
+
+const configureRoutes = (app: Express) => {
+  app.use('/api/v1/auth', authRouter);
+  app.use('/api/v1/recipes', recipesRoute);
+  app.use('/api/v1/bookmarks', bookmarksRoute);
+  app.use('/api/v1/reset', resetRoute);
+};
+
+export default configureRoutes
