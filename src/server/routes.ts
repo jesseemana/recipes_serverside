363e51a4a99379fb1b379d70e078638d4f9e8c42
Jesse Emana <jesseemana@gmail.com>
2023-11-10 19:27:23 +0200

    REFACTOR: changed endpoint from reset route to user route

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,11 +2,11 @@ import { Express } from 'express'
 import authRouter from '../routes/auth.route'
 import recipesRoute from '../routes/recipes.route'
 import bookmarksRoute from '../routes/bookmarks.route'
-import resetRoute from '../routes/reset.route'
+import resetRoute from '../routes/user.route'
 
 const configureRoutes = (app: Express) => {
   app.use('/api/v1/auth', authRouter);
-  app.use('/api/v1/recipes', recipesRoute);
+  app.use('/api/v1/user', recipesRoute);
   app.use('/api/v1/bookmarks', bookmarksRoute);
   app.use('/api/v1/reset', resetRoute);
 };
