a499d8e07541ad99295931b897fabda0156f373c
Jesse Emana <jesseemana@gmail.com>
2023-11-10 19:25:41 +0200

    REFACTOR: moved create user handler from auth route to user route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express'
 import loginLimiter from '../middleware/login-limiter'
-import { createSessionHandler, createUserHandler, destroySessionHandler, refreshTokenHandler } from '../controllers/auth.controller'
+import { createSessionHandler, destroySessionHandler, refreshTokenHandler } from '../controllers/auth.controller'
 import validateInput from '../middleware/validateInput'
-import { createSessionSchema, createUserSchema } from '../schema/user.schema'
+import { createSessionSchema } from '../schema/user.schema'
 import requireUser from '../middleware/require-user'
 
 const router = Router()
 
-router.post('/register', validateInput(createUserSchema), createUserHandler)
 router.post('/login', validateInput(createSessionSchema), loginLimiter, createSessionHandler)
 router.get('/refresh', refreshTokenHandler)
 router.post('/logout', requireUser, destroySessionHandler)
