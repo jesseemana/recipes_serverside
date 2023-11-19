bdf35ed663a78a9c47d3488648dd5c4555a85d7c
Jesse Emana <jesseemana@gmail.com>
2023-11-19 06:18:23 +0200

    REFACTOR: added file naming(renaming) functionality

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,16 +1,15 @@
 import multer, { FileFilterCallback } from 'multer'
 import path from 'path'
-import { Request, } from 'express'
 
 const upload = multer({
   storage: multer.diskStorage({
-    filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+    filename: (_, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
       let ext = path.extname(file.originalname)
-      cb(null, 'IMG' + Date.now() + ext)
+      cb(null, `IMG${Date.now}${ext}`)
     }
   }),
 
-  fileFilter: (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+  fileFilter: (_, file: Express.Multer.File, cb: FileFilterCallback) => {
     let ext = path.extname(file.originalname)
     if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
       cb(new Error('File type is not supported'))
