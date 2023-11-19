20e399405739dd58a6f201b9801f20fffea1522f
Jesse Emana <jesseemana@gmail.com>
2023-11-20 00:13:06 +0200

    REFACTOR: final routes setup

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -5,10 +5,11 @@ import bookmarksRoute from '../routes/bookmarks.route'
 import userRoute from '../routes/user.route'
 import sessionsRoute from '../routes/session.route'
 
+
 const configureRoutes = (app: Express) => {
   app.use('/api/v1/sessions', sessionsRoute);
   app.use('/api/v1/auth', authRoute);
-  app.use('/api/v1/recipe', recipesRoute);
+  app.use('/api/v1/recipes', recipesRoute);
   app.use('/api/v1/bookmarks', bookmarksRoute);
   app.use('/api/v1/user', userRoute);
 };
