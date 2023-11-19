9b220a95c6f46ec676e9c66c3482e4c9a968c9b5
Jesse Emana <jesseemana@gmail.com>
2023-11-20 00:11:34 +0200

    REFCATOR: put all recipes related handlers in one route

--- src/routes/recipes.route.ts
+++ src/routes/recipes.route.ts
@@ -4,14 +4,19 @@ import { createRecipeHandler, updateRecipeHandler, deleteRecipeHandler } from '.
 import requireUser from '../middleware/require-user'
 import validateInput from '../middleware/validateInput'
 import { updateRecipeSchema } from '../schema/recipe.schema'
+import { getAllRecipesHandler, getSingleRecipeHandler, getUserRecipesHandler } from '../controllers/get-recipe.controller'
 
 const router = Router()
 
 router.route('/')
+  .get(getAllRecipesHandler)
   .post(requireUser, [upload.single('file')], createRecipeHandler)
 
 router.route('/:id')
+  .get(getSingleRecipeHandler)
   .patch([requireUser, validateInput(updateRecipeSchema)], updateRecipeHandler)
   .delete(requireUser, deleteRecipeHandler)
+
+router.get('/user/:user_id', getUserRecipesHandler)
 
 export default router  
