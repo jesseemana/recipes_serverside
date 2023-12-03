8a8795080938cbed0f102571ed435873c9b40254
Jesse Emana <jesseemana@gmail.com>
2023-12-03 12:13:45 +0200

    CHORE: defined types for the HttpCode

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,17 +1,14 @@
+import { HttpCode } from '../../types'
+
 export class AppError extends Error {
   public readonly name: string
-  public readonly httpCode: HtppCode 
+  public readonly httpCode: HttpCode 
   public readonly isOperational: boolean
 
-  constructor(
-    name: string, 
-    httpCode: HtppCode, 
-    description: string, 
-    isOperational: boolean
-  ) {
+  constructor(name: string, httpCode: HttpCode, description: string, isOperational: boolean) {
     super(description)
 
-    Object.setPrototypeOf(this, new.target.prototype)
+    Object.setPrototypeOf(this, new.target.prototype) // restore prototype chain
 
     this.name = name
     this.httpCode = httpCode
