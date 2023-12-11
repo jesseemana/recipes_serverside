6c370e4036db8315a0e1022337a03f0628bfad78
Jesse Emana <jesseemana@gmail.com>
2023-12-11 13:22:03 +0200

    CHORE: added middleware for recording promethius metrics

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 require('dotenv').config()
 require('express-async-errors')
-import express from 'express'
+import express, { Request, Response } from 'express'
+import responseTime from 'response-time'
 import configureRoutes from './server/routes'
 import configureServer from './server/configure'
 import initializeServer from './server/initialize-app'
 import database from './utils/connect-db'
+import { restResponseTimeHistogram } from './utils/metrics'
 
 const app = express()
 
 configureServer(app)
+
+// recording promethius metrics 
+app.use(responseTime((req: Request, res: Response, time: number) => {
+  if (req.route.path) {
+    restResponseTimeHistogram.observe(
+      {
+        method: req.method,
+        route: req.route.path,
+        status_code: res.statusCode
+      }, 
+      time * 1000
+    )
+  }
+}))
 
 configureRoutes(app)
 
