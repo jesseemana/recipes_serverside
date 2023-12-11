9df4a745d6f6bdcde2b5def4a812f66f354b4cd3
Jesse Emana <jesseemana@gmail.com>
2023-12-11 19:22:31 +0200

    CHORE: sanitizing user input

--- src/routes/recipes.route.ts
+++ src/routes/recipes.route.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import upload from '../middleware/multer'
 import { createRecipeHandler, updateRecipeHandler, deleteRecipeHandler } from '../controllers/recipe.controller'
 import requireUser from '../middleware/require-user'
-import validateInput from '../middleware/validateInput'
+import validateInput from '../middleware/validate-input'
 import { updateRecipeSchema } from '../schema/recipe.schema'
 import { getAllRecipesHandler, getSingleRecipeHandler, getUserRecipesHandler } from '../controllers/get-recipe.controller'
 
