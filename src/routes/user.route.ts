bcd4e399b298d2f2244ffb029985a367e89b2e89
Jesse Emana <jesseemana@gmail.com>
2024-01-19 17:51:31 +0200

    REFACTOR: reformatted code

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,12 +2,22 @@ import { Router } from 'express'
 import { UserController } from '../controllers'
 import { validateInput }from '../middleware'
 import { createUserSchema } from '../schema/user.schema'
-import { resetAuthSchema, updateAuthSchema } from '../schema/reset.schema'
+import { updateAuthSchema } from '../schema/reset.schema'
 
 const router = Router()
 
-router.post('/register', validateInput(createUserSchema), UserController.createUserHandler)
+router.post(
+  '/register', 
+  validateInput(createUserSchema), 
+  UserController.createUserHandler
+)
+
+router.patch(
+  '/reset/:id/:token', 
+  validateInput(updateAuthSchema), 
+  UserController.resetPasswordHandler
+)
+
 router.post('/forgot_password', UserController.forgortPasswordHandler)
-router.patch('/reset/:id/:token', validateInput(updateAuthSchema), UserController.resetPasswordHandler)
 
 export default router 
