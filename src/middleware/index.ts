0182e849315630b6588b34cafc5a209a2b380d4f
Jesse Emana <jesseemana@gmail.com>
2024-01-22 15:43:51 +0200

    REFACTOR: changed typing case

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,15 +1,15 @@
 import upload from "./multer";
-import requireUser from "./require-user";
-import errorHandler from "./error-handler";
-import loginLimiter from "./login-limiter";
-import validateInput from "./validate-input";
+import require_user from "./require-user";
+import error_handler from "./error-handler";
+import login_limiter from "./login-limiter";
+import validate_input from "./validate-input";
 import deserialize_user from "./auth.middleware";
 
 export {
   upload,
-  loginLimiter,
-  requireUser,
-  errorHandler,
-  validateInput,
+  login_limiter,
+  require_user,
+  error_handler,
+  validate_input,
   deserialize_user,
 }
