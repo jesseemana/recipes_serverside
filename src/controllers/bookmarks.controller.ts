a26a44ee8a8958934a8dd0b99623ccca3ef980ef
Jesse Emana <jesseemana@gmail.com>
2024-01-22 20:08:09 +0200

    REFACTOR: changed param name for getting user bookmarks

--- src/controllers/bookmarks.controller.ts
+++ src/controllers/bookmarks.controller.ts
@@ -1,26 +1,24 @@
 import { Request, Response } from 'express';
 import { AppError } from '../utils';
 import { UserService, RecipeService } from '../services';
-import { HandleBookmarksInput, GetBookmarksInput } from '../schema/bookmarks.schema';
+import { BookmarksInput } from '../schema/bookmarks.schema';
 
 
 const userBookmarksHandler = async (
-  req: Request<GetBookmarksInput, {}, {}>, 
+  req: Request<BookmarksInput, {}, {}>, 
   res: Response
 ) => {
-  const { id } = req.params;
+  const { user_id } = req.params;
 
-  const user = await UserService.findUserById(id);
-  if (!user) return res.status(404).send('User does not exist')
+  const user = await UserService.findUserById(user_id);
+  if (!user) return res.status(404).send('User does not exist');
   // if (!user) throw new AppError('Not Found', 404, 'User not found', true);
 
   const bookmarks = [];
 
   for (const bookmark of user.bookmarks) {
     const recipe = await RecipeService.findRecipeById(bookmark);
-    if (recipe) {
-      bookmarks.push(recipe);
-    }
+    if (recipe) { bookmarks.push(recipe); }
   }
 
   res.status(200).send(bookmarks);
@@ -28,21 +26,25 @@ const userBookmarksHandler = async (
 
 
 const addBookmarkHandler = async (
-  req: Request<HandleBookmarksInput, {}, {}>, 
+  req: Request<BookmarksInput, {}, {}>, 
   res: Response
 ) => {
   const { user_id, recipe_id } = req.params;
 
   const found_user = UserService.findUserById(user_id);
   const found_recipe = RecipeService.findRecipeById(recipe_id);
+
   const [user, recipe] = await Promise.all([found_user, found_recipe]);
 
-  if (!recipe || !user) return res.status(404).send('User or recipe not found')
+  if (!recipe || !user) { 
+    return res.status(404).send('User or recipe not found');
     // throw new AppError('Not Found', 404, 'User or Recipe does not exist', true);
+  }
 
-  if (user.bookmarks.includes(recipe_id)) 
-    return res.status(400).send('Recipe already bookmarked')
+  if (user.bookmarks.includes(recipe_id)) {
+    return res.status(400).send('Recipe already bookmarked');
     // throw new AppError('Bad Request', 400, 'Recipe is already bookmarked', true);
+  }
 
   user.bookmarks.push(recipe_id);
   await user.save();
@@ -52,21 +54,24 @@ const addBookmarkHandler = async (
 
 
 const removeBookmarkHandler = async (
-  req: Request<HandleBookmarksInput, {}, {}>, 
+  req: Request<BookmarksInput, {}, {}>, 
   res: Response
 ) => {
   const { user_id, recipe_id } = req.params;
 
   const found_user = UserService.findUserById(user_id);
   const found_recipe = RecipeService.findRecipeById(recipe_id);
+
   const [user, recipe] = await Promise.all([found_user, found_recipe]);
-
-  if (!recipe || !user) return res.status(404).send('User or recipe not found')
+  if (!recipe || !user) {
+    return res.status(404).send('User or recipe not found');
     // throw new AppError('Not Found', 404, 'User or Recipe does not exist', true);
+  }
 
-  if (!user.bookmarks.includes(recipe_id)) 
-    return res.status(400).send(`Can't perform operation, recipe is not bookmarked`)
+  if (!user.bookmarks.includes(recipe_id)) {
+    return res.status(400).send(`Can't perform operation, recipe is not bookmarked`);
     // throw new AppError('Bad Request', 400, `Can't perform operation, recipe is not bookmarked`, true);
+  }
 
   user.bookmarks = [...(user.bookmarks.filter((bookmark) => bookmark !== recipe_id))];
   await user.save();
@@ -79,4 +84,4 @@ export default {
   removeBookmarkHandler, 
   addBookmarkHandler, 
   userBookmarksHandler,
-}
+};
