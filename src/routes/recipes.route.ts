d86cc7a35c4dbeb95fac4b2f22c06c19540f2bdd
Jesse Emana <jesseemana@gmail.com>
2024-01-25 19:40:38 +0200

    CHORE: minor fixes

--- src/routes/recipes.route.ts
+++ src/routes/recipes.route.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express'
 import { updateRecipeSchema } from '../schema/recipe.schema'
-import { upload, requireUser, validateInput } from '../middleware'
+import { upload, require_user, validate_input } from '../middleware'
 import { RecipeController, GetRecipeController } from '../controllers'
 
 const router = Router()
 
 router.route('/')
   .get(GetRecipeController.getAllRecipesHandler)
-  .post(requireUser, [upload.single('file')], RecipeController.createRecipeHandler)
+  .post(
+    [require_user, upload.single('file')], 
+    RecipeController.createRecipeHandler,
+  )
 
 router.route('/:id')
   .get(GetRecipeController.getSingleRecipeHandler)
-  .patch([requireUser, validateInput(updateRecipeSchema)], RecipeController.updateRecipeHandler)
-  .delete(requireUser, RecipeController.deleteRecipeHandler)
+  .delete(require_user, RecipeController.deleteRecipeHandler)
+  .patch(
+    [require_user, validate_input(updateRecipeSchema)], 
+    RecipeController.updateRecipeHandler,
+  )
 
 router.get('/user/:user_id', GetRecipeController.getUserRecipesHandler)
 
