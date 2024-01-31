f8e4802549f3580d95506b6fa32814e14b4cfbf9
Jesse Emana <jesseemana@gmail.com>
2024-01-31 21:29:37 +0200

    REFACTOR: removed pino pretty in logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,9 +6,6 @@ const level = config.get<string>('logLevel')
 
 // NB: DON'T USE PINO PRETTY IN PRODUCTION
 const log = logger({
-  transport: {
-    target: 'pino-pretty'
-  },
   level,
   base: {
     pid: false
