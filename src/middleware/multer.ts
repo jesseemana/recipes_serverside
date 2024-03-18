84c5114bd5e4c13d339c08907b87ee99bb3daa46
Jesse Emana <jesseemana@gmail.com>
2024-03-18 12:04:10 +0200

    REFACTOR: fixed filname

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,26 +1,26 @@
-import multer, { FileFilterCallback } from 'multer'
-import path from 'path'
+import multer, { FileFilterCallback } from 'multer';
+import path from 'path';
 
 const upload = multer({
   storage: multer.diskStorage({
-    filename: (_, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-      let ext = path.extname(file.originalname)
-      cb(null, `IMG${Date.now}${ext}`)
+    filename: (_req, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+      let ext = path.extname(file.originalname);
+      cb(null, `IMG_${Date.now()}${ext}`);
     }
   }),
 
-  fileFilter: (_, file: Express.Multer.File, cb: FileFilterCallback) => {
-    let ext = path.extname(file.originalname)
+  fileFilter: (_req, file: Express.Multer.File, cb: FileFilterCallback) => {
+    let ext = path.extname(file.originalname);
     if (
       ext !== '.jpg' && 
       ext !== '.png' && 
       ext !== '.jpeg'
     ) {
-      cb(new Error('Choose a supported file type.'))
-      return 
+      cb(new Error('Choose a supported file type.'));
+      return;
     }
-    cb(null, true)
+    cb(null, true);
   }
-})  
+})
 
-export default upload
+export default upload;
