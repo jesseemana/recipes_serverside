25ed4e8bc41d7715bcb14db38878d033d6cd73f3
Jesse Emana <jesseemana@gmail.com>
2024-03-18 20:53:48 +0200

    REFACTOR: minor changes

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,13 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import { Jwt } from '../utils';
 
 const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
-  const token = (req.headers.authorization || "").split(' ')[1].trim();
-  if (!token) return next();
-  const public_key = String(process.env.ACCESS_TOKEN_PUBLIC_KEY);
-  const decoded = Jwt.verifyToken<{}>(token, public_key);
-  if (decoded) { 
-    res.locals.user = decoded; // alternative to req.user to avoid TS errors
-  } 
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    const token = req.headers.authorization.split(' ')[1].trim();
+    if (!token) return next();
+    const public_key = String(process.env.ACCESS_TOKEN_PUBLIC_KEY);
+    const decoded = Jwt.verifyToken<{}>(token, public_key);
+    if (decoded) { 
+      res.locals.user = decoded; // alternative to req.user to avoid TS errors
+    } 
+  }
   return next();
 }
 
