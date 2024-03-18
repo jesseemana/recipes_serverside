960b97cc019a560d7f0c9e17bea41ae6a0acce24
Jesse Emana <jesseemana@gmail.com>
2024-03-18 20:54:14 +0200

    CHORE: Swagger docs

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -7,90 +7,88 @@ const router = Router();
 
 const { findSessionsHandler, createSessionHandler, refreshTokenHandler, destroySessionHandler } = AuthController;
 
-// /**
-//  * @openapi
-//  * '/api/v2/auth/sessions':
-//  * get:
-//  *    tags:
-//  *    - Auth
-//  *    summary: Get sessions
-//  *    responses:
-//  *      200:
-//  *        description: Success
-//  *        content:
-//  *          application/json:
-//  *            schema:
-//  *              $ref: '#/components/schema/SessionResponse'
-//  *      401:
-//  *        description: Unauthorized
-//  *      403:
-//  *        description: Forbidden
-//  */
+/**
+ * @openapi
+ * '/api/v2/auth/sessions':
+ *  get:
+ *    tags:
+ *    - Auth
+ *    summary: Get sessions
+ *    responses:
+ *      200:
+ *        description: Returns all sessions in database
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schema/SessionResponse'
+ *      401:
+ *        description: Unauthorized
+ *      403:
+ *        description: Forbidden
+ */
 router.get('/sessions', requireUser, findSessionsHandler);
 
-// /**
-//  * @openapi
-//  *'/api/v2/auth/login':
-//  *  post:
-//  *      tags: 
-//  *      - Auth
-//  *      summary: Login user
-//  *      requestBody:
-//  *        required: true
-//  *        content:
-//  *          application/json:
-//  *            schema:
-//  *              $ref: '#/components/schema/loginUserInput'
-//  *      responses:
-//  *        200:
-//  *          description: Success
-//  *          content:
-//  *            application/json:
-//  *              schema:
-//  *                $ref: '#/components/schema/loginResponse'
-//  *        404:
-//  *          description: Invalid email/user not found
-//  *        401:
-//  *          description: Invalid password
-//  */
+/**
+ * @openapi
+ *'/api/v2/auth/login':
+ *  post:
+ *      tags: 
+ *      - Auth
+ *      summary: Login user
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schema/loginUserInput'
+ *      responses:
+ *        200:
+ *          description: Creates a new user session
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schema/loginResponse'
+ *        401:
+ *          description: Invalid email/password
+ */
 router.post('/login', [validateInput(createSessionSchema), loginLimiter], createSessionHandler);
 
-// /**
-//  * @openapi
-//  * '/api/v2/auth/refresh':
-//  * get:
-//  *    tags:
-//  *    - Auth
-//  *    summary: Refresh token
-//  *    responses:
-//  *      200:
-//  *        description: Success
-//  *        content:
-//  *          application/json:
-//  *            schema:
-//  *              $ref: '#/components/schema/RefreshResponse'
-//  *      401:
-//  *        description: Refersh token/Session not found or is invalid
-//  *      403:
-//  *        description: No refresh token found
-//  *      404:
-//  *        description: User not found
-//  */
+/**
+ * @openapi
+ * '/api/v2/auth/refresh':
+ *  get:
+ *    tags:
+ *    - Auth
+ *    summary: Refresh token
+ *    responses:
+ *      200:
+ *        description: Returns a refresh token
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schema/RefreshResponse'
+ *      401:
+ *        description: Refresh token/session not found or is invalid
+ *      403:
+ *        description: No refresh token found
+ *      404:
+ *        description: User not found
+ */
 router.get('/refresh', refreshTokenHandler);
 
-// /**
-//  * @openapi
-//  * '/api/v2/auth/logout':
-//  *  delete:
-//  *        tags:
-//  *        - Auth
-//  *        summary: Logout user
-//  *        responses:
-//  *          200:
-//  *              description: User loged out
-//  *          403:
-//  *              description: Forbidden
-//  */
+/**
+ * @openapi
+ * '/api/v2/auth/logout':
+ *  delete:
+ *        tags:
+ *        - Auth
+ *        summary: Logout user
+ *        responses:
+ *          200:
+ *              description: User loged out
+ *          403:
+ *              description: Forbidden
+ */
 router.delete('/logout', requireUser, destroySessionHandler);
 
 export default router;
