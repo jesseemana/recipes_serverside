511c644f4752865859edcbe8a17e63bc5b9822fd
Jesse Emana <jesseemana@gmail.com>
2024-03-20 07:10:44 +0200

    REFACTOR: fixed next function

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,12 +4,12 @@ import { Jwt } from '../utils';
 export const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
     const token = req.headers.authorization.split(' ')[1].trim();
-    if (!token) return next();
     const public_key = String(process.env.ACCESS_TOKEN_PUBLIC_KEY);
     const decoded = Jwt.verifyToken<{}>(token, public_key);
     if (decoded) { 
       res.locals.user = decoded; // alternative to req.user to avoid TS errors
-    } 
+    }
+    return next();
   }
   return next();
 }
