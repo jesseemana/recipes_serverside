cbc1773f4b3a097dc860631a79c4cf8faf2e04f0
Jesse Emana <jesseemana@gmail.com>
2024-03-23 01:38:44 +0200

    REFACTOR: moved handlers for getting recipes into

--- src/controllers/recipe.controller.ts
+++ src/controllers/recipe.controller.ts
@@ -1,7 +1,86 @@
 import { Request, Response } from 'express';
-import { RecipeService } from '../services';
+import { RecipeService, UserService } from '../services';
 import { AppError, uploadPicture } from '../utils';
-import { CreateRecipeInput, UpdateRecipeInput, } from '../schema/recipe.schema';
+import { CreateRecipeInput, GetUserRecipeInput, UpdateRecipeInput, } from '../schema/recipe.schema';
+
+const ITEMS_PER_PAGE = 20;
+
+const getAllRecipesHandler = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string) || 1;
+  const skip = (page - 1) * ITEMS_PER_PAGE; // starting index of every page
+  const count = await RecipeService.totalRecipes();
+
+  const recipes = await RecipeService.getAllRecipes(ITEMS_PER_PAGE, skip);
+  if (!recipes.length) return res.status(404).send('No recipes found.');
+    // throw new AppError('Not Found', 404, 'There are no recipes found. Create some.', true);
+
+  const recipes_with_user = await Promise.all(recipes.map(async(recipe) => {
+    const user = await UserService.findUserById(String(recipe.user));
+    if (!user) return res.status(404).send(`User doesn't have any recipes.`);
+      // throw new AppError('Not Found', 404, `User doesn't have any recipes.`, true);
+
+    return { 
+      ...recipe, 
+      username: `${user.first_name} ${user.last_name}`,
+    }
+  }));
+
+  return res.status(200).json({
+    recipes: recipes_with_user,
+    pagination: {
+      page: page,
+      total_pages: Math.ceil(count / ITEMS_PER_PAGE),
+    }
+  });
+}
+
+
+const getUserRecipesHandler = async (
+  req: Request<GetUserRecipeInput['params'], {}, {}>, 
+  res: Response
+) => {
+  const { user_id } = req.params;
+
+  const page = parseInt(req.query.page as string) || 1;
+  const skip = (page - 1) * ITEMS_PER_PAGE; // starting index of every page
+  const total = await RecipeService.totalRecipes();
+
+  const user = await UserService.findUserById(user_id);
+  if (!user) return res.status(404).send('User not found.');
+    // throw new AppError('Not Found', 404, `User not found.`, true); 
+    
+  const recipes = await RecipeService.getUserRecipes(user_id, ITEMS_PER_PAGE, skip);
+  if (!recipes.length) return res.status(404).send(`User doesn't have any recipes..`);
+    // throw new AppError('Not Found', 404, `User doesn't have any recipes..`, true);
+  const full_name = `${user.first_name} ${user.last_name}`;
+
+  return res.status(200).json({ 
+    recipes: recipes, 
+    full_name: full_name, 
+    pagination: {
+      page: page,
+      tota_pages: Math.ceil(total / ITEMS_PER_PAGE),
+    }
+  });
+}
+
+
+const getSingleRecipeHandler = async (
+  req: Request<UpdateRecipeInput['params'], {}, {}>, 
+  res: Response
+) => {
+  const { id } = req.params;
+
+  const recipe = await RecipeService.findRecipeById(id);
+  if (!recipe) return res.status(404).send('Recipe not found.');
+    // throw new AppError('Not Found', 404, `Recipe not found.`, true) ;
+  const user = await UserService.findUserById(String(recipe.user));
+  if (!user) return res.status(404).send('User not found.');
+    // throw new AppError('Not Found', 404, `User not found.`, true) ;
+  const owner = `${user.first_name} ${user.last_name}`;
+  
+  return res.status(200).json({ recipe, owner, });
+}
 
 
 const createRecipeHandler = async (
@@ -19,7 +98,7 @@ const createRecipeHandler = async (
     return res.status(400).send('Please provide a picture');
   } catch (error) {
     return res.status(500).send('Internal server error.');
-    // throw new AppError('Internal Server Error', 500, 'Something went wrong', false)
+    // throw new AppError('Internal Server Error', 500, 'Something went wrong', false);
   }
 }
 
@@ -61,13 +140,17 @@ const deleteRecipeHandler = async (
     return res.status(401).send('User cannot make this operation.');
     // throw new AppError('Unauthorized', 401, 'User cannot this operation', true);
 
-  await RecipeService.deleteRecipe(id, recipe.cloudinary_id);
+  const deleted = await RecipeService.deleteRecipe(id, recipe.cloudinary_id);
+  if (deleted) return res.status(200).send('Recipe has been deleted!');
 
-  return res.status(200).send('Recipe has been deleted!');
+  return res.status(400).send('Failed to delete recipe.')
 }
 
 
 export default {
+  getAllRecipesHandler,
+  getUserRecipesHandler,
+  getSingleRecipeHandler,
   createRecipeHandler,
   updateRecipeHandler,
   deleteRecipeHandler,
--- src/routes/recipes.route.ts
+++ src/routes/recipes.route.ts
@@ -1,22 +1,22 @@
 import { Router } from 'express';
 import { upload, requireUser, validateInput } from '../middleware';
-import { RecipeController, GetRecipeController } from '../controllers';
+import { RecipeController } from '../controllers';
 import { createRecipeSchema, updateRecipeSchema } from '../schema/recipe.schema';
 
 const router = Router();
 
-const { createRecipeHandler, updateRecipeHandler, deleteRecipeHandler } = RecipeController;
-const { getSingleRecipeHandler, getAllRecipesHandler, getUserRecipesHandler } = GetRecipeController;
-
 router.route('/')
-  .get(getAllRecipesHandler)
-  .post([requireUser, validateInput(createRecipeSchema), upload.single('file')], createRecipeHandler,);
+  .get(RecipeController.getAllRecipesHandler)
+  .post(
+    [requireUser, validateInput(createRecipeSchema), upload.single('file')], 
+    RecipeController.createRecipeHandler
+  );
 
 router.route('/:id')
-  .get(getSingleRecipeHandler)
-  .patch([requireUser, validateInput(updateRecipeSchema)], updateRecipeHandler)
-  .delete(requireUser, deleteRecipeHandler);
+  .get(RecipeController.getSingleRecipeHandler)
+  .patch([requireUser, validateInput(updateRecipeSchema)], RecipeController.updateRecipeHandler)
+  .delete(requireUser, RecipeController.deleteRecipeHandler);
 
-router.get('/user/:user_id', getUserRecipesHandler);
+router.get('/user/:user_id', RecipeController.getUserRecipesHandler);
 
 export default router;
