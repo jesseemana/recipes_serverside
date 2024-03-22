43d7e9d6b439660c2759dd62488ab86e9ac40c84
Jesse Emana <jesseemana@gmail.com>
2024-03-23 01:36:12 +0200

    REFACTOR: removed getrecipes controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,11 @@
 import AuthController from "./auth.controller";
-import BookmarksController from "./bookmarks.controller";
-import GetRecipeController from "./get-recipe.controller";
 import RecipeController from "./recipe.controller";
+import BookmarksController from "./bookmarks.controller";
 import UserController from "./user.controller";
 
 export {
   AuthController,
-  BookmarksController,
-  GetRecipeController,
   RecipeController,
+  BookmarksController,
   UserController,
 }
