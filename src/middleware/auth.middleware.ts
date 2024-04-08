4c2f18d0e6bf4c3b57321588cdb82492adcd2dc4
Jesse Emana <jesseemana@gmail.com>
2024-04-08 13:26:53 +0200

    REFACTOR: minor changes

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,13 +4,13 @@ import { Jwt } from '../utils';
 export const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
 
-  if (authorization && authorization.startsWith('Bearer ')) {
-    const token = authorization.split(' ')[1].trim();
-    const public_key = String(process.env.ACCESS_TOKEN_PUBLIC_KEY);
-    const decoded = Jwt.verifyToken<{}>(token, public_key);
-    if (decoded) { res.locals.user = decoded; } // alternative to req.user to avoid TS errors
-    next();
-  }
-  
+  if (!authorization || !authorization.startsWith('Bearer ')) return next();
+
+  const token = authorization.split(' ')[1].trim();
+  const decoded = Jwt.verifyToken<{}>(token, String(process.env.ACCESS_TOKEN_PUBLIC_KEY));
+  if (decoded) { 
+    res.locals.user = decoded; // alternative to req.user to avoid TS errors
+  } 
+
   next();
 }
--- src/middleware/require-user.ts
+++ src/middleware/require-user.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 
 export const requireUser = (_req: Request, res: Response, next: NextFunction) => {
   const user = res.locals.user;
-  if (!user) return res.sendStatus(403);
+  if (!user) {
+    return res.sendStatus(401);
+  }
   next();
 }
