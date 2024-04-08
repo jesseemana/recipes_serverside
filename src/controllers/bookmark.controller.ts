ce14ddc7e74a057887bf800a8df4a0f56ce92fc4
Jesse Emana <jesseemana@gmail.com>
2024-04-08 13:32:09 +0200

    CHORE: created bookmarks controller and service

---
+++ src/controllers/bookmark.controller.ts
@@ -0,0 +1,109 @@
+import { Request, Response } from 'express';
+import { BookmarksInput } from '../schema/bookmarks.schema';
+import { BookmarkService, UserService, RecipeService } from '../services';
+
+
+async function userBookmarksHandler(
+  req: Request<BookmarksInput, {}, {}>, 
+  res: Response
+) {
+  const { user_id } = req.params;
+
+  const limit = 20;
+  const page = parseInt(req.query.page as string) || 1;
+  const skip = (page - 1) * limit;
+  const total = await BookmarkService.countUserBookmarks(user_id);
+
+  const user = await UserService.findUserById(user_id);
+  if (!user) {
+    return res.status(404).send('User not found.');
+  }
+
+  const user_bookmarks = await BookmarkService.getUserBookmarks(String(user._id), limit, skip);
+  
+  const bookmarks = await Promise.all(user_bookmarks.map(async (bookmark) => {
+    const recipe = await RecipeService.findRecipeById(String(bookmark.recipe));
+    if (!recipe) return;
+    return recipe;
+  }));
+
+  return res.status(200).send({
+    bookmarks: bookmarks,
+    pagination: {
+      page: page,
+      total_pages: Math.ceil(total / limit),
+    }
+  });
+}
+
+
+async function addBookmarkHandler(
+  req: Request<BookmarksInput, {}, {}>, 
+  res: Response
+) {
+  const { user_id, recipe_id } = req.params;
+
+  const user = UserService.findUserById(user_id);
+  const recipe = RecipeService.findRecipeById(recipe_id);
+
+  const [found_user, found_recipe] = await Promise.all([user, recipe]);
+  if (!found_user || !found_recipe) {
+    return res.status(404).send('User or recipe not found.');
+  }
+
+  const isBookmarked = await BookmarkService.isBookmarked({ 
+    user_id: user_id, 
+    recipe_id: recipe_id, 
+  });
+
+  if (isBookmarked) return res.status(401).send('Recipe is already bookmarked by user.');
+
+  const bookmark = await BookmarkService.createBookmark({ 
+    user: found_user._id, 
+    recipe: found_recipe._id 
+  });
+
+  return res.status(200).json({
+    message: 'Bookmark added.',
+    bookmark: bookmark,
+  });
+}
+
+
+async function removeBookmarkHandler(
+  req: Request<BookmarksInput, {}, {}>, 
+  res: Response
+) {
+  const { user_id, recipe_id } = req.params;
+
+  const user = UserService.findUserById(user_id);
+  const recipe = RecipeService.findRecipeById(recipe_id);
+
+  const [found_user, found_recipe] = await Promise.all([user, recipe]);
+  if (!found_user || !found_recipe) {
+    return res.status(404).send('User or recipe not found.');
+  }
+
+  const isBookmarked = await BookmarkService.isBookmarked({ 
+    user_id: user_id, 
+    recipe_id: recipe_id, 
+  });
+
+  if (!isBookmarked) return res.status(401).send('Recipe is not bookmarked by user.');
+
+  const removed = await BookmarkService.removeBookmark({ 
+    user_id: user_id, 
+    recipe_id: recipe_id, 
+  });
+
+  return res.status(200).json({
+    message: 'Bookmark removed.',
+    bookmark: removed,
+  });
+}
+
+export default {
+  userBookmarksHandler, 
+  addBookmarkHandler, 
+  removeBookmarkHandler, 
+}
---
+++ src/services/bookmark.service.ts
@@ -0,0 +1,55 @@
+import { BookmarkModel } from '../models';
+import { Bookmark } from '../models/bookmark.model';
+
+type BookmarkType = {
+  user_id: string, 
+  recipe_id: string,
+}
+
+const countUserBookmarks = async (user_id: string) => {
+  const count = await BookmarkModel.estimatedDocumentCount({ user: user_id });
+  return count;
+}
+
+const isBookmarked = async ({ user_id, recipe_id }: BookmarkType) => {
+  const bookmarked = await BookmarkModel.findOne({ 
+    user: user_id, 
+    recipe: recipe_id, 
+  });
+
+  if (bookmarked) return true;
+  
+  return false;
+}
+
+const getUserBookmarks = async (user_id: string, limit: number, index: number) => {
+  const bookmarks = await BookmarkModel.find({ user: user_id })
+    .sort({ createdAt: 'desc' })
+    .skip(index)
+    .limit(limit)
+    .exec();
+
+  return bookmarks;
+}
+
+const createBookmark = async (data: Bookmark) => {
+  const bookmark = await BookmarkModel.create(data);
+  return bookmark;
+}
+
+const removeBookmark = async ({ user_id, recipe_id }: BookmarkType) => {
+  const removed = await BookmarkModel.findOneAndDelete({ 
+    user: user_id, 
+    recipe: recipe_id, 
+  });
+
+  return removed;
+}
+
+export default { 
+  getUserBookmarks, 
+  createBookmark, 
+  isBookmarked,
+  countUserBookmarks, 
+  removeBookmark, 
+}
