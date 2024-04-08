14abf2d2c5297c6ae64942a26c2858c94404dd48
Jesse Emana <jesseemana@gmail.com>
2024-04-08 13:31:23 +0200

    REFACTOR: minor changes

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,14 +1,17 @@
 import { omit } from 'lodash';
 import { Jwt } from '../utils';
-import { SessionModel } from '../models';
-import { Session } from '../models/session.model';
 import { DocumentType } from '@typegoose/typegoose';
 import { FilterQuery, UpdateQuery } from 'mongoose';
-import { User, private_fields } from '../models/user.model';
+import { User } from '../models/user.model';
+import { SessionModel } from '../models';
+import { Session } from '../models/session.model';
 
 
 const findAllSessions = async () => {
-  const sessions = await SessionModel.find({});
+  const sessions = await SessionModel.find({})
+    .limit(10)
+    .sort({ createdAt: -1 });
+
   return sessions;
 }
 
@@ -23,26 +26,35 @@ const findSessionById = async (id: string) => {
   return session;
 }
 
-const destroySession = async (query: FilterQuery<Session>, update: UpdateQuery<Session>) => {
+async function destroySession(
+  query: FilterQuery<Session>, 
+  update: UpdateQuery<Session>
+) {
   await SessionModel.findOneAndUpdate(query, update);
   return true;
 }
 
-const signAccessToken = async (user: DocumentType<User>, session: DocumentType<Session>) => {
-  const user_payload = omit(user.toJSON(), private_fields);
-  const private_key = String(process.env.ACCESS_TOKEN_PRIVATE_KEY);
+const signAccessToken = (user: DocumentType<User>) => {
+  const user_payload = omit(user.toJSON(), 'password');
   const time_to_live = String(process.env.ACCESS_TOKEN_TIME_TO_LIVE);
 
-  const access_token = Jwt.signJwt({ ...user_payload, session }, private_key, { expiresIn: time_to_live });
+  const access_token = Jwt.signJwt(
+    { user_payload }, // decode token(online) and check payload
+    String(process.env.ACCESS_TOKEN_PRIVATE_KEY), 
+    { expiresIn: time_to_live }
+  );
 
   return access_token;
 }
 
 const signRefreshToken = async (session: DocumentType<Session>) => {
-  const private_key = String(process.env.REFRESH_TOKEN_PRIVATE_KEY);
   const time_to_live = String(process.env.REFRESH_TOKEN_TIME_TO_LIVE);
   
-  const refresh_token = Jwt.signJwt({ session: session._id }, private_key, { expiresIn: time_to_live });
+  const refresh_token = Jwt.signJwt(
+    { session: session._id }, 
+    String(process.env.REFRESH_TOKEN_PRIVATE_KEY), 
+    { expiresIn: time_to_live }
+  );
 
   return refresh_token;
 }
@@ -51,7 +63,7 @@ export default {
   findAllSessions,
   destroySession,
   findSessionById,
-  createSession,
   signAccessToken,
+  createSession,
   signRefreshToken,
 }
